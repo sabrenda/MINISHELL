6321127f94133de06825d47c978d0a3d9466a2a3
Akhmed A <nob_32@mail.ru>
2021-04-12 23:34:41 +0300

    ༼ つ ◕_◕ ༽つ

--- main.c
+++ main.c
@@ -16,37 +16,8 @@ int	ft_lenmassive(char **str)
 
 	i = 0;
 	while (str[i])
-	{
 		i++;
-	}
 	return (i);
-}
-
-char *ft_dir(char **myenv)
-{
-	int	i;
-	int	j;
-	int tmp;
-	char *line;
-
-	j = -1;
-	i = -1;
-	while(myenv[++i])
-		if (strncmp("PWD=", myenv[i], 4) == 0)
-			break;
-	while (myenv[i][++j])
-		if (myenv[i][j] == '/')
-			tmp = j;
-	j = 0;
-	line = (char *)malloc(sizeof(char) * 1000);
-	while (myenv[i][tmp])
-	{
-		line[j] = myenv[i][tmp];
-		j++;
-		tmp++;
-	}
-	line[j] = '\0';
-	return (line);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -54,22 +25,20 @@ int	main(int argc, char **argv, char **env)
 	char	*line;
 	int		status;
 	int		i;
-	char	*dir;
 	t_monna	lisa;
 
-	i = ft_lenmassive(env);
-	lisa.my_env = (char **)malloc(sizeof(char *) * i + 1);
-	lisa.my_env[i] = NULL;
+	lisa.my_env = (char **)malloc(sizeof(char *) * ft_lenmassive(env) + 1);
+	lisa.my_env[ft_lenmassive(env)] = NULL;
 	i = -1;
 	while (env[++i])
 		lisa.my_env[i] = ft_strdup(env[i]);
 	i = -1;
 	lisa.status = 1;
-	dir = ft_dir(lisa.my_env);
 	while (lisa.status)
 	{
+		ft_putstr_fd("\033[31m༼ つ ◕_◕ ༽つ\033[32m$ ", 1);
 		get_next_line(1, &line); //чтение ввода
-		printf("%s | (^_^)> %s\n", dir, line);
+		printf("%s\n", line);
 		// parser(line, &lisa); //парсим строку
 		if (strcmp("exit", line) == 0)
 		{
@@ -79,6 +48,5 @@ int	main(int argc, char **argv, char **env)
 		free(line);
 		// lisa.status = executor(&lisa, env); выполнение
 	}
-	i = -1;
 	return (0);
 }
