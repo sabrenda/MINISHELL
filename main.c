f8e41708cb93a1313247b9ca2e64a41825af7801
Akhmed A <nob_32@mail.ru>
2021-04-13 01:23:20 +0300

    DaVinci

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include "libft/libft.h"
 #include "libft/get_next_line.h"
 
+void	ml(char *monna, int lisa);
+
 typedef struct s_monna
 {
 	char	**commands;
@@ -9,6 +11,83 @@ typedef struct s_monna
 	char	**tokens;
 	int		status;
 }				t_monna;
+
+void	ml(char *monna, int lisa)
+{
+	write(lisa, monna, ft_strlen(monna));
+}
+
+void	ft_monnalisa(void)
+{
+	ml("i`it)v|[[[[(//s+)`(-///JJgbdd@@@@@@@dmKK(c!(/-[2=/cct/!-v//_L//|\n", 1);
+	ml("]-!/(!-)//L)v|c5(!,!Ldd@@@@@@@@@@@@@@@@@@@dK/]!c///v|i//cT//((c-\n", 1);
+	ml("]!`/v///(-|t//vcL!m@@@@@M@@@@@@@@@@@@@@@@@@@@bLt//|)c/2-vv)/it//\n", 1);
+	ml("--/-,/////|)/v/m@@@@@@K@@@@@@@@@@@@@@@@@@@@@@@bK!v!-( )-!.[../cT\n", 1);
+	ml("//.//-'--|-/c(e@@@@@@@DD@@@@@@@@@@@@@@@@@@@@@@@@@@s////-||/v!c//\n", 1);
+	ml("-,-|//||/-//id@@@@@@@@N@@@@@@@@@@@@@@@@@@@@@@@@@@@@b.),`-,-/c-`i\n", 1);
+	ml("!,/!-!-!'!-!d@@@@@@@P[+~**AAA@@@@@@@@@@@@@@@@@@@@@@@b/./`c-/.//-\n", 1);
+	ml("--'.-- -/,id@@@*P!`          /'Z8@@@@@@@@@@@@@@@@@@@@@i.//'....c\n", 1);
+	ml("',`,`/'-,-J@@5`-           -- `-iYA@@@@@@@@@@b@@@@@@@@@_-|'.;-c-\n", 1);
+	ml("'. -.,`/.G@@K- `               - )7KM@@@@@@@@@@@@@@@@@@@c---..-/\n", 1);
+	ml("- `-  --i@@Ai                    -!ZZ@@@@@@@@@@@@@@@@@@@b! `|-.`\n", 1);
+	ml(" `-,'- G@@@[,                    '.D8K@@@@@@@@@@@@@@@@@@@[/-,-/.\n", 1);
+	ml("-` .-/v@@@A)                      -)ZdMd@@@@@@@@@@@@@@@@@@/' _//\n", 1);
+	ml("- ` ,iVJ@@@!                     '-!(K5K@@@@@@@@@@@@@@@@@@[(/s[.\n", 1);
+	ml("  - i/G@@@Z-                    ' ! -i55ZZ@@@@@@@@@@@@@@@@@)(4)`\n", 1);
+	ml(" , -|b@@@@!@                     '  ` |-tYG@@@@@@@@@@@@@@@@XNYZ-\n", 1);
+	ml("   tt@@@@A-,                        '  `)(d@@@@@@@@@@@@@@@@D)8A[\n", 1);
+	ml("   )8@@@@@//                         ,-'-/Kd@@@@@@@@@@@@@@@@KD@[\n", 1);
+	ml("  ]]Z@@@@d|-              ,ii.c,, -.icLZKK@@@@@@8@K@@@@@@@@@(@8[\n", 1);
+	ml("  KN8@@@@@( .i!vGG_      J4Kb8ZKb@bbK@d@88@@@@@@@b@@@@@@@@@@dK@-\n", 1);
+	ml(" )/8K@@@K@b@dP~~~T4(    Jd@@7`___s@M@@@@MM8d@@@d@@@@@@@@@@@@LM8[\n", 1);
+	ml("/!48@K@@@@8@@d*@@@bVi   bAKLY~~@@@@@@*ff//NM8@@@@@@@@@@@@@@@db@[\n", 1);
+	ml(",//Kb@@@d@.~t` !*~!`.  -MA)    '~'.).` `,'K@@@@@@@@@@@@@@@@@AKb[\n", 1);
+	ml(",`8M@@@@@@ -`,,gvZ``    A//-  ..c/+/`    i]d@@@@@@M@@@@@@@@@@@8[\n", 1);
+	ml("i/@8@K@@@D              /!'             !iZ8@@@8A@@@@@8d@b@@@8M[\n", 1);
+}
+
+void	ft_monnalisa_2(void)
+{
+	ml("e8d5@@@@@@             '!-             '-)8@@@@@@@@@@@@@@@@@@M8i\n", 1);
+	ml("8dZ8@M@@@@-             v  ,          ,//K@@@@@@@@@@@@A@@@@@@Z2|\n", 1);
+	ml("@b@AK@@@b@[              //           cctbA@@@AK@@d@@d@@@K@@@bmi\n", 1);
+	ml("@@8@M@8@@@P-            -=/.         /iD8d@@@@@@@@@@@@@@A@@@d@@[\n", 1);
+	ml("@8@@@MA@@@@/-      .   _)g2i        -((dKK@@@@@d@@@@d@@K@@@@@@K[\n", 1);
+	ml("@@@bAK@@K@@)i     'c,,Kb@@bK       )X)Kb@M@@d@@@Mb@@A@d@@@@@@8@[\n", 1);
+	ml("@K@b@@@@A@AA/i-     ~M@@@@Mc    .,/c=)D8d@@b@@@d@@@@@@@@@@8d@@A[\n", 1);
+	ml("@@@@Mb@@@@@@('c/`     PPK((,i]v|-/-v)8XNAdMK@@@@@@@b@@MK@A@@@@@[\n", 1);
+	ml("@@8@@MK@d@A@L!--c)s_, ,(ZsbLb@/`- .-N]/KM@@@@@@@d@@@A@@@@@@@@d@[\n", 1);
+	ml("@@Kb@@@K@b@@@/-  !''~~Vff*N5f -` -,/))KK@@@@@@@MK@@d@@@M8d@b@@@[\n", 1);
+	ml("@b@@@KAK@@@@@@2--    ,,_JJ/i)/- |/v)NK@8d@@@@@@@@@@8@@@@@@@@M@K[\n", 1);
+	ml("@@8d@K@@@b@@@@@d!,   'VV/)//)//(-)4Jb@8@A@@@K@d@@@@@@@8@@@@@@@@[\n", 1);
+	ml("M@@@@8@@K@Kb@@@d@v.       `-///v)88b@M@A@K@@M@@@A@@M@8@@A@d@8@M[\n", 1);
+	ml("Zb@d@M@K@@@@@@@@@@m       -)!/stbb@b@@A@b@@@@@Kb@@@@@@@b@@@K@@@[\n", 1);
+	ml("K@@d@@@@@d@M@8@@@@@Ks   ,-/vJD@@8d@K@@@@@@8@@@@@@@@@@MK@@@b@@M@[\n", 1);
+	ml("tN@b@@d@d@M@@@@@@@@@@LL4JKd@A@@d@@K@@@@MK@@@@8@@@@@@@@@@@b@@@@@[\n", 1);
+	ml(")NM@8b@@A@@@A@@@@@@@@@@@@@@A@@A@@8@@K@d@@@@M@@K@@K@A@@@8@@M@@@@[\n", 1);
+	ml("(tMM@@@d@@M8@@@@A@@@@A@@@A@@@@@@@@@A@@@@8b@@8d@@@@@@@@@@@@@@@@M[\n", 1);
+	ml("tNZ@@K@@@d@@@@A@@@@@8@@@/4N@@8@b@@d@@M@8@MK@M8@K@@@@@@d@@@@@@@@[\n", 1);
+	ml("M/KA8@@@MA@@@M@@@@@@@@@@[|t*Z@N@@@@8@@M8ZAZZ@M@@@A@d@@@@@K@@@d@[\n", 1);
+}
+
+void	ft_monnalisa_3(void)
+{
+	ml("bYJ4M@@@@@@A@@@@@@@@@@@@D.//(YKKZD@8dK@52.", 1);
+	ml("\033[35m..@Tchariss..\033[0m@@@@@@@@[\n", 1);
+	ml("K5dM8@8d@d@@@@@@@@8@@@@@@..-!/))ZK5AY1231.", 1);
+	ml("\033[31m..@Sabrenda..\033[0m@@@@@@@@[\n", 1);
+	ml("Y8dNA@@AK@@d@@@b@@@@@@@@L,-,/!]]/X/7@12@@.", 1);
+	ml("\033[32m..MiniShell..\033[0m@@@@@@@@[\n", 1);
+	ml("8M8@@@A@@@A@@8@@@@@@@@@KDLt! !,-|//123///.", 1);
+	ml("\033[36m..MonnaLisa..\033[0m@@@@@@@@[\n", 1);
+}
+
+void	ft_davinci(void)
+{
+	ft_monnalisa();
+	ft_monnalisa_2();
+	ft_monnalisa_3();
+}
 
 int	ft_lenmassive(char **str)
 {
@@ -27,6 +106,7 @@ int	main(int argc, char **argv, char **env)
 	int		i;
 	t_monna	lisa;
 
+	ft_davinci();
 	lisa.my_env = (char **)malloc(sizeof(char *) * ft_lenmassive(env) + 1);
 	lisa.my_env[ft_lenmassive(env)] = NULL;
 	i = -1;
