efc673efb72f8a4a5de1c2e1b975b6253626b4f5
Tennie Charissa <tchariss@ox-l3.msk.21-school.ru>
2021-04-15 21:23:32 +0300

    3

---
+++ ft_echo.c
@@ -0,0 +1,55 @@
+#include <string.h>
+#include "libft/libft.h"
+#include "libft/get_next_line.h"
+#include <errno.h>
+#include <signal.h>
+#include <termios.h>
+#include <stdio.h>
+# include <curses.h>
+# include <term.h>
+
+char *ft_pwd(char *str)
+{
+    char *s1;
+
+    s1 = getcwd(NULL, 0);
+    strlcat(s1, "\n", ft_strlen(s1) + 2);
+    return (s1);
+}
+
+void ft_echo(char **str)
+{
+    int i = 1;
+    int fl_cmd = 0;
+
+    if(strcmp("-n", str[1]) == 0)
+        ft_putstr_fd(NULL, 1);
+    else if(*str[1] == '$')
+    {
+        // ft_putstr_fd("$-tut", 1);
+        // printf("s:%s\n", *str);
+        // printf("s1:%s\n", str[i++]);
+        // printf("c:%c\n", *str[i]);
+        fl_cmd = 1;
+        if(strcmp("$PWD", str[i++]) == 0)
+        {
+            ft_putstr_fd(getcwd(NULL, 0), 1);
+            ft_putstr_fd("\n", 1);
+        }
+    }
+    else
+    {
+        ft_putstr_fd(str[1], 1);
+        ft_putstr_fd("\n", 1);        
+    }
+}
+
+int main(void)
+{
+    char *ar[2];
+
+    ar[0] = "echo";
+    // ar[1] =  "echo";
+
+    ft_echo(ar);
+}
