4c717afe8729e0b1f4b1868b87d53d556a5aa8d8
Akhmed A <nob_32@mail.ru>
2021-04-16 22:56:04 +0300

    Parser 99.9% + comments

--- main.c
+++ main.c
@@ -78,13 +78,13 @@ void	ft_monnalisa_2(void)
 	m("!!!!>        c$$$$$$$$$$\033[32m|..MiniShell..|\033[0m$$$$$      \n", 1);
 }
 
-void	ft_davinci(void)
+void	ft_davinci(void)// Leonardo di ser Piero da Vinci
 {
-	ft_monnalisa();
-	ft_monnalisa_2();
+	ft_monnalisa();		// Ritratto di Monna Lisa del Giocondo
+	ft_monnalisa_2();	// 1503—1519
 }
 
-int	ft_lenmassive(char **str)
+int	ft_lenmassive(char **str) // счетчик строк в 2-ном массиве
 {
 	int	i;
 
@@ -94,83 +94,83 @@ int	ft_lenmassive(char **str)
 	return (i);
 }
 
-void	ft_len_kov(t_pars *len, char *line)
+void	ft_len_kov(t_pars *len, char *line) // ковычки для счетчика слов
 {
 	len->count = 0;
-	len->c = line[len->i];
+	len->c = line[len->i]; //копрую ковычку " или '
 	while (1)
 	{
-		len->i++;
-		len->count++;
-		if (line[len->i] == '\0')
+		len->i++; //пропуск первой ковычки
+		len->count++; //счетчик символов внутри ковычек
+		if (line[len->i] == '\0') //если встретился \0 до ковычки, создаем вторую
 		{
-			line[len->i++] = len->c;
-			line[len->i] = '\0';
+			line[len->i++] = len->c; //вместо \0 добавляем ковычку
+			line[len->i] = '\0'; // добавляем конец строки
 			if (len->count > 1)
-				len->flag = 0;
+				len->flag = 0; // флаг для счетчика слов
 			break ;
 		}
-		if (line[len->i] == len->c)
+		if (line[len->i] == len->c) // если нашел вторую ковычку
 		{
-			len->i++;
+			len->i++; //пропуск ковычки
 			if (len->count > 1)
-				len->flag = 0;
+				len->flag = 0; // флаг для счетчика слов
 			break ;
 		}
 	}
 }
-void	ft_len_kov_pars(t_pars *pars, char *line, t_monna *lisa)
+void	ft_len_kov_pars(t_pars *pars, char *line, t_monna *lisa) // ковычки парсер
 {
 	pars->count = 0;
-	pars->c = line[pars->i];
+	pars->c = line[pars->i]; //копрую ковычку " или '
 	while (1)
 	{
-		pars->i++;
-		pars->count++;
-		if (line[pars->i] == pars->c)
+		pars->i++; //пропуск первой ковычки
+		pars->count++; //счетчик символов внутри ковычек
+		if (line[pars->i] == pars->c) // если нашел вторую ковычку
 		{
-			pars->i++;
+			pars->i++; // пропуск ковычки
 			if (pars->count > 1)
-				pars->flag = 0;
+				pars->flag = 0; //флаг для счетчика слов
 			break ;
 		}
-		lisa->tokens[pars->word][pars->j++] = line[pars->i];
+		lisa->tokens[pars->word][pars->j++] = line[pars->i]; //добавляет при нахождении символов
 	}
 }
 
-void ft_len_alpha(char *line, t_pars *len)
+void ft_len_alpha(char *line, t_pars *len) // пропуск символов и ковычки (для счетчика слов)
 {
 	while (line[len->i] && ((line[len->i] == '\"' || line[len->i] == '\'')
 		|| (line[len->i] != ' ' && line[len->i] != '\t')))
 	{
 		if (line[len->i] == '\"' || line[len->i] == '\'')
-			ft_len_kov(len, line);
+			ft_len_kov(len, line); // работа с ковычками (счетчик слов)
 		while(line[len->i] != ' ' && line[len->i] != '\t' && line[len->i]
 			&& line[len->i] != '\"' && line[len->i] != '\'')
 		{
 			len->i++;
-			len->flag = 0;
+			len->flag = 0; //флаг для счетчика слов
 		}
 	}
 }
 
-void ft_len_alpha_pars(char *line, t_pars *pars, t_monna *lisa)
+void ft_len_alpha_pars(char *line, t_pars *pars, t_monna *lisa) //для добавляния символов и ковычки
 {
 	while (line[pars->i] && ((line[pars->i] == '\"' || line[pars->i] == '\'')
 		|| (line[pars->i] != ' ' && line[pars->i] != '\t')))
 	{
 		if (line[pars->i] == '\"' || line[pars->i] == '\'')
-			ft_len_kov_pars(pars, line, lisa);
+			ft_len_kov_pars(pars, line, lisa); // работа с ковычками (парсер)
 		while(line[pars->i] != ' ' && line[pars->i] != '\t' && line[pars->i]
 			&& line[pars->i] != '\"' && line[pars->i] != '\'')
 		{
-			lisa->tokens[pars->word][pars->j++] = line[pars->i++];
+			lisa->tokens[pars->word][pars->j++] = line[pars->i++]; // добавляем в наш массив символы
 			pars->flag = 0;
 		}
 	}
 }
 
-int	ft_len_space_tab(char *line, t_pars *len)
+int	ft_len_space_tab(char *line, t_pars *len) //пропуск табов и спэйсов
 {
 	while (line[len->i] == '\t' || line[len->i] == ' ')
 		len->i++;
@@ -179,7 +179,7 @@ int	ft_len_space_tab(char *line, t_pars *len)
 	return (1);
 }
 
-int	ft_len_words(char *line)
+int	ft_len_words(char *line) // считает колличество слов в строке из гнл с учетом ковычек
 {
 	t_pars	len;
 
@@ -188,10 +188,10 @@ int	ft_len_words(char *line)
 	{
 		len.flag = 1;
 		if (line[len.i] == '\t' || line[len.i] == ' ')
-			if (!(ft_len_space_tab(line, &len)))
+			if (!(ft_len_space_tab(line, &len))) //пропуск ковычек
 					break ;
 		if (line[len.i] != '\t' && line[len.i] != ' ')
-			ft_len_alpha(line, &len);
+			ft_len_alpha(line, &len); // пропуск букв и работа с ковычками
 		if (len.flag == 0)
 			len.word++;
 		if (line[len.i] == '\0')
@@ -200,7 +200,7 @@ int	ft_len_words(char *line)
 	return (len.word);
 }
 
-int ft_memory_pars(t_monna *lisa, char *line, t_pars	*pars)
+int ft_memory_pars(t_monna *lisa, char *line, t_pars *pars)
 {
 	int	i;
 
@@ -216,18 +216,18 @@ int ft_memory_pars(t_monna *lisa, char *line, t_pars	*pars)
 			return (0);
 		i++;
 	}
-	if (ft_len_words(line) == 0)
+	if (ft_len_words(line) == 0) // если слов (символов) == 0
 		lisa->tokens[i] = NULL;
 	pars->i = 0;
 	pars->word = -1;
 	return (1);
 }
 
-int	parser(char *line, t_monna *lisa)
+int	parser(char *line, t_monna *lisa) //обрабтка строки из гнл
 {
 	t_pars	pars;
 
-	if (ft_memory_pars(lisa, line, &pars) == 0)
+	if (ft_memory_pars(lisa, line, &pars) == 0)// выделяем память для двумерного массива
 		return (0);
 	while (++pars.word < ft_len_words(line) && line[pars.i])
 	{
@@ -236,10 +236,10 @@ int	parser(char *line, t_monna *lisa)
 		while (line[pars.i] && pars.flag)
 		{
 			if (line[pars.i] == '\t' || line[pars.i] == ' ')
-				if (!(ft_len_space_tab(line, &pars)))
+				if (!(ft_len_space_tab(line, &pars))) //пропускаем space и табуляции
 					break ;
 			if (line[pars.i] != '\t' && line[pars.i] != ' ')
-				ft_len_alpha_pars(line, &pars, lisa);
+				ft_len_alpha_pars(line, &pars, lisa); // добавляем символы и работа с ковычками
 		}
 		lisa->tokens[pars.word][pars.j] = '\0';
 	}
